4bf50f5e1b48857c074d4cbc02396d81fc78241c
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-07-16 21:45:50 +0900

    adfadfd

--- api/server.js
+++ api/server.js
@@ -16,19 +16,23 @@ const authUser = {
   description:
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
 };
+
 server.use(cookieParser());
 server.use(express.json());
+
 server.post('/auth/signin', (req, res) => {
   if (!(req.body['username'] === 'user' && req.body['password'] === 'password')) {
     return res.status(401).json({message: 'Username or password are incorrect',
     });
   }
+  //tokenの設定を行う
   res.cookie('token', 'dummy_token', {
     maxAge: 3600 * 1000,
     httpOnly: true,
   });
   res.status(201).json(authUser);
 });
+
 server.post('/auth/signout', (req, res) => {
   res.cookie('token', '', {
     maxAge: 0,
@@ -38,12 +42,14 @@ server.post('/auth/signout', (req, res) => {
     message: 'Sign out successfully',
   });
 });
+
 server.post('/purchases', (req, res) => {
   if (req.cookies['token'] !== 'dummy_token') {
     return res.status(401).json({
       message: 'UnauthorizedOOOOOOOOOOOOOO',
     });
   }
+
   res.status(201).json({
     message: 'ok',
   });
@@ -52,16 +58,16 @@ server.post('/purchases', (req, res) => {
 //初期画面描画時に実行される
 //errorStates が404になる理由がある
 // errorBodyにmessageが本来はいるが入っていない ===> if文が実行されていない
+//users/meに対してのget request
 server.get('/users/me', (req, res) => {
   if (req.cookies['token'] !== 'dummy_token') {
     return res.status(401).json({
       message: 'Unauthorized /users/me',
     });
   }
-  message:"これはエラーです"
-
   res.status(200).json(authUser);
 });
+
 server.use(middlewares);
 server.use(router);
 server.listen(port, (err) => {
@@ -71,5 +77,4 @@ server.listen(port, (err) => {
     return;
   }
   console.log("Start listening...");
-  console.log('http://localhost:' + port);
 });
