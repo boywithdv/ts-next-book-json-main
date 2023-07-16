81dfe53a7c0b57a85f55bc88c6f5bb3f6818b54e
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-07-16 21:39:54 +0900

    dafd

--- api/server.js
+++ api/server.js
@@ -16,23 +16,19 @@ const authUser = {
   description:
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
 };
-
 server.use(cookieParser());
 server.use(express.json());
-
 server.post('/auth/signin', (req, res) => {
   if (!(req.body['username'] === 'user' && req.body['password'] === 'password')) {
     return res.status(401).json({message: 'Username or password are incorrect',
     });
   }
-  //tokenの設定を行う
   res.cookie('token', 'dummy_token', {
     maxAge: 3600 * 1000,
     httpOnly: true,
   });
   res.status(201).json(authUser);
 });
-
 server.post('/auth/signout', (req, res) => {
   res.cookie('token', '', {
     maxAge: 0,
@@ -42,23 +38,20 @@ server.post('/auth/signout', (req, res) => {
     message: 'Sign out successfully',
   });
 });
-
 server.post('/purchases', (req, res) => {
   if (req.cookies['token'] !== 'dummy_token') {
     return res.status(401).json({
       message: 'UnauthorizedOOOOOOOOOOOOOO',
     });
   }
-
   res.status(201).json({
     message: 'ok',
   });
 });
-
 //初期画面描画時に実行される
 //errorStates が404になる理由がある
 // errorBodyにmessageが本来はいるが入っていない ===> if文が実行されていない
-//users/meに対してのget request
+//users/meエンドポイントに対してのget request
 server.get('/users/me', (req, res) => {
   if (req.cookies['token'] !== 'dummy_token') {
     return res.status(401).json({
@@ -67,7 +60,6 @@ server.get('/users/me', (req, res) => {
   }
   res.status(200).json(authUser);
 });
-
 server.use(middlewares);
 server.use(router);
 server.listen(port, (err) => {
@@ -77,4 +69,5 @@ server.listen(port, (err) => {
     return;
   }
   console.log("Start listening...");
+  console.log('http://localhost:' + port);
 });
