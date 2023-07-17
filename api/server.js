62887a87f5a4b5c49542e1e1c99415f36d2df607
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-07-17 17:17:05 +0900

    messageを/api/proxy/users/meに変更する

--- api/server.js
+++ api/server.js
@@ -56,7 +56,7 @@ server.post('/purchases', (req, res) => {
 server.get('/users/me', (req, res) => {
   if (req.cookies['token'] !== 'dummy_token') {
     return res.status(401).json({
-      message: 'Unauthorized',
+      message: 'Unauthorized /api/proxy/users/me',
     });
   }
 
