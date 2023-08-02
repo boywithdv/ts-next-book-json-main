b4d650fa0d00dea3084bc7965d2b9f87935d5c5e
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-08-02 09:31:17 +0900

    single('fileの削除

--- api/server.js
+++ api/server.js
@@ -72,10 +72,14 @@ server.get('/api/proxy/users/me', (req, res) => {
 //ここから追加
 const upload = multer({ storage });
 //ファイルのアップロードを処理するエンドポイント
-server.post('/api/proxy/upload', upload.single('file'), (req, res) => {
+server.post('/api/proxy/upload', (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: "No file uploaded" });
   }
+  //保存したファイルのパスを公開URLにする
+  const publicUrl = `/upload/${req.file.filename}.png`;
+  console.log('これがファイルのURLです : ',`${publicUrl}`)
+  res.json({ url: `${publicUrl}` });
 })
 
 server.use(middlewares);
