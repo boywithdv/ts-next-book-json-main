044155f316ca1ce3628a01f3f29ba7ab16bfb485
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-08-02 19:15:17 +0900

    commit

--- api/server.js
+++ api/server.js
@@ -95,7 +95,7 @@ const upload = multer({ storage });
  * しかし公開した場合にはエラーとなる
  */
 
-server.post('/api/proxy/product', upload.single('file'), (req, res) => {
+server.post('/api/proxy/products', upload.single('file'), (req, res) => {
   console.log("111これが req.body : ", req.body)
   const filePath = path.join("/tmp", "data.json");
   fs.writeFileSync(filePath, JSON.stringify(req.body));
