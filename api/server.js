ac23ec50c4fa45c5eb83347c20b0d39bb64ae16a
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-08-03 09:43:02 +0900

    commit

--- api/server.js
+++ api/server.js
@@ -94,18 +94,10 @@ const upload = multer({ storage });
 /**
  * しかし公開した場合にはエラーとなる
  */
-const filePath = path.join("/tmp", "db.json");
-server.post('/api/proxy/product', upload.single('file'), (req, res) => {
-  console.log("111これが req.body : ", req.body)
-  //この2行を追加してパスの設定ができるようにした
-  fs.writeFileSync(filePath, JSON.stringify(req.body));
-  console.log('filePathは ',filePath)
-  //保存したファイルのパスを公開URLにする /upload/${req.file.filename}.png
+server.post('/api/proxy/product', (req, res) => {
   const publicUrl = `${req.body.imageUrl}`;
   console.log('これがファイルのURLです : ', `${publicUrl}`)
   res.status(200).json(req.body)
-  //res.status(200).json({url : publicUrl});
-  //res.json({ url: `${publicUrl}` });
 })
 server.use(middlewares);
 server.use(router);
