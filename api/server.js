4c68cc324bfc691e6d64f9cbdee343ab72f4f76f
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-08-03 12:13:10 +0900

    commit

--- api/server.js
+++ api/server.js
@@ -123,6 +123,7 @@ export default function handler(req, res) {
       res.status(201).json({ message: 'Product added successfully!', data: productData });
     } catch (err) {
       console.error('Failed to add product:', err);
+      // 製品の追加に失敗
       res.status(500).json({ error: 'Failed to add product' });
     }
   } else {
