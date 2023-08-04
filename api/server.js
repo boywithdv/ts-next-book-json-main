07290986305ed06b0e20727f6cc00a19d4ec45c4
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-08-04 16:45:12 +0900

    commit

--- api/server.js
+++ api/server.js
@@ -90,8 +90,8 @@ server.post('/api/proxy/products', upload.single('file'), (req, res) => {
   console.log("111これが req.body : ",req.body)
   const { product } = req.body; // クライアントから送られたProductデータ
   // db.jsonに新しいProductデータを追加する
-  const dbPath = '/db.json';
-  const dbData = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+  const dbPath = 'db.json';
+  const dbData = JSON.parse(fs.readFileSync(dbPath));
   dbData.products.push(product);
   // db.jsonを更新する
   fs.writeFileSync(dbPath, JSON.stringify(dbData, null, 2));
