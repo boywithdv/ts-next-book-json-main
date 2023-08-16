c1c979ca9df6e372b56f4c1e39b7a43390aacbe8
j.tsukuda <j.tsukuda@nas-tec.co.jp>
2023-08-16 14:32:36 +0900

    result削除

--- api/server.js
+++ api/server.js
@@ -90,8 +90,6 @@ server.post('/api/proxy/products', (req, res) => {
   dbData.products.push(req.body);
   // db.jsonを更新する
   fs.writeFileSync('/tmp/db.json', JSON.stringify(dbData));
-  const result = JSON.parse(fs.readFileSync(dbPath))
-  console.log(result.products)
   res.status(200).json(req.body)
   //res.status(200).json({url : publicUrl});
   //res.json({ url: `${publicUrl}` });
